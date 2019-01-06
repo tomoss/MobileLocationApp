37bb41df46079d06613f10959ef285e26fccb865
Unknown <ionut.tomoss@gmail.com>
2019-01-06 17:05:26 +0200

    login and register design done

--- app/src/main/java/org/scd/mobiletrackingapp/LoginActivity.java
+++ app/src/main/java/org/scd/mobiletrackingapp/LoginActivity.java
@@ -1,14 +1,37 @@
 package org.scd.mobiletrackingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtEmail;
+    EditText edtPassword;
+    Button btnLogin;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
+
+
 }
--- app/src/main/java/org/scd/mobiletrackingapp/RegisterActivity.java
+++ app/src/main/java/org/scd/mobiletrackingapp/RegisterActivity.java
@@ -8,6 +8,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
     }
 }
